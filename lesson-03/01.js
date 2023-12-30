cee4a59c749fbb6af7a49ce4672d36aff2e5eea9
Dima <dimaberry1@gmail.com>
2023-12-30 21:48:43 +0100

    feat: solve return german flag

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+let colors = ['синий', 'черный', 'зеленый', 'красный', 'желтый', 'зеленый'];
+
+function createColorString() {
+  return colors.filter((e) => e === 'черный' || e === 'красный' || e === 'желтый').join('-');
+}
+
+console.log(createColorString());
